ead0c52466dc45b58e525f60134bf8915cfa9af0
Pieter Otten <pieter.otten@signify.com>
2021-09-22 14:23:38 +0200

    Added support for the 3 new Bluetooth permissions of Android 12.

--- permission_handler_platform_interface/lib/src/permissions.dart
+++ permission_handler_platform_interface/lib/src/permissions.dart
@@ -157,6 +157,20 @@ class Permission {
   ///iOS: Nothing
   static const accessNotificationPolicy = Permission._(27);
 
+  ///Android: Allows the user to look for Bluetooth devices
+  ///(e.g. BLE peripherals).
+  ///iOS: Nothing
+  static const bluetoothScan = Permission._(28);
+
+  ///Android: Allows the user to make this device discoverable to other
+  ///Bluetooth devices.
+  ///iOS: Nothing
+  static const bluetoothAdvertise = Permission._(29);
+
+  ///Android: Allows the user to connect with already paired Bluetooth devices.
+  ///iOS: Nothing
+  static const bluetoothConnect = Permission._(30);
+
   /// Returns a list of all possible [PermissionGroup] values.
   static const List<Permission> values = <Permission>[
     calendar,
@@ -187,6 +201,9 @@ class Permission {
     appTrackingTransparency,
     criticalAlerts,
     accessNotificationPolicy,
+    bluetoothScan,
+    bluetoothAdvertise,
+    bluetoothConnect,
   ];
 
   static const List<String> _names = <String>[
@@ -218,6 +235,9 @@ class Permission {
     'appTrackingTransparency',
     'criticalAlerts',
     'accessNotificationPolicy',
+    'bluetoothScan',
+    'bluetoothAdvertise',
+    'bluetoothConnect',
   ];
 
   @override
